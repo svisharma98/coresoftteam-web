702c1b2a9ec526b3bf1c61380172c2614c98e20d
vishwajeet <svisharma98@gmail.com>
2025-06-29 13:19:53 +0530

    Setup CST

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page | Core Soft Team",
+  description: "This is About Page for Core Soft Team",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
+  );
+};
+
+export default AboutPage;
